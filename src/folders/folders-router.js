b376007502dbbd842f869e2a468a301f09ecbcc9
Angela Masajo <aabmasajo@gmail.com>
2021-02-20 14:24:23 -0600

    GET / and /:folderId

--- src/folders/folders-router.js
+++ src/folders/folders-router.js
@@ -4,7 +4,7 @@ const xss = require('xss')
 const logger = require('../logger')
 const FoldersService = require('./folders-service')
 
-const articlesRouter = express.Router()
+const foldersRouter = express.Router()
 const jsonParser = express.json()
 
 const serializeFolder = folder => ({
@@ -15,7 +15,7 @@ const serializeFolder = folder => ({
 
 foldersRouter
   .route('/')
-  .get((res, res, next) => {
+  .get((req, res, next) => {
     const knexInstance = req.app.get('db')
     FoldersService.getAllFolders(knexInstance)
       .then(folders => {
@@ -24,5 +24,25 @@ foldersRouter
       .catch(next)
   })
 
-
-module.exports = articlesRouter
+foldersRouter
+  .route('/:folderId')
+  .all((req, res, next) => {
+    FoldersService.getById(
+      req.app.get('db'),
+      req.params.folderId
+    )
+      .then(folder => {
+        if (!folder) {
+          return res.status(400).json({
+            error: { message: `Folder doesn't exist` }
+          })
+        }
+        res.folder = folder
+        next()
+      })
+      .catch(next)
+  })
+  .get((req, res, next) => {
+    res.json(serializeFolder(res.folder))
+  })
+module.exports = foldersRouter
--- src/folders/folders-service.js
+++ src/folders/folders-service.js
@@ -4,6 +4,13 @@ const FoldersService = {
       .select('*')
       .from('folders')
   },
+  getById(knex, id) {
+    return knex 
+      .from('folders')
+      .select('*')
+      .where('id', id)
+      .first()
+  },
 }
 
 module.exports = FoldersService
